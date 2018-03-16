48a8ce4d5a934b060b104425c7055288b3da2129
PRIGOOSE <PRIYALGHOSE@GMAIL.COM>
2018-03-16 16:01:37 -0700

    -Passed first tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,10 @@
 var app = {
-  init: () => true,
+  init: function() {
+    $('form').on('submit', app.handleSubmit);
+  },
   send: function(data) {
     $.ajax({
-  // This is the url you should use to communicate with the parse API server.
+      // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
       data: JSON.stringify(data),
@@ -18,7 +20,7 @@ var app = {
   },
   fetch: function(data) {
     $.ajax({
-  // This is the url you should use to communicate with the parse API server.
+      // This is the url you should use to communicate with the parse API server.
       url: app.server,
       type: 'GET',
       data: JSON.stringify(data),
@@ -33,23 +35,26 @@ var app = {
     });
   },
   clearMessages: function() {
-    $('#chats').empty()
+    $('#chats').empty();
   }, 
   renderMessage: function(message) {
-    $message = '<p>' + message.text + '</p>';
+    $message = '<div><p class="username">' + message.username + '</p><p id="message">' + message.text + '</p><p class="room">' + message.roomname + '</p></div>';
     $('#chats').append($message);
+    $('.username').on('click', app.handleUsernameClick);
   },
   renderRoom: function(room) {
     $room = '<p>' + room + '</p>';
     $('#roomSelect').append($room);
-  }
+  },
+  handleUsernameClick: () => true,
+  handleSubmit: () => true
 };
 
 var message = {
   username: 'shawndrost',
   text: 'trololo',
   roomname: '4chan',
-}
+};
  
 $.ajax({
   // This is the url you should use to communicate with the parse API server.
