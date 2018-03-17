1720f717158a82a19019ab4c71173bf9256fd374
PRIGOOSE <PRIYALGHOSE@GMAIL.COM>
2018-03-16 20:01:07 -0700

    Did some stuff, potentially fixed a few bugs, or created a lot more... Overall, 7/10

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,12 @@
+// $(document).ready(function() {
+
 var app = {
   init: function() {
-    $('form').on('submit', app.handleSubmit);
+    $('form').on('submit', app.handleSubmit);    
+    app.fetch();
+    // var oldMessages = app.fetch();
+    // console.log(oldMessages);
+    $('.newTweets').on('click', app.addNewTweets);
   },
   send: function(data) {
     $.ajax({
@@ -18,15 +24,18 @@ var app = {
       }
     });
   },
-  fetch: function(data) {
+  fetch: function() {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: app.server,
       type: 'GET',
-      data: JSON.stringify(data),
+      // data: 'order=-createdAt',
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        for (var i = 0; i < data.results.length; i++) {
+          app.renderMessage(data.results[i]);
+        }
+        $('.username').on('click', app.handleUsernameClick);
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -38,51 +47,49 @@ var app = {
     $('#chats').empty();
   }, 
   renderMessage: function(message) {
-    $message = '<div><p class="username">' + message.username + '</p><p id="message">' + message.text + '</p><p class="room">' + message.roomname + '</p></div>';
+    message.roomname = message.roomname || 'No room specified';
+    message.text = message.text || 'No message specified';
+    message.username = message.username || 'No username specified';
+    $message = $('<div class="tweet"><p class="username">' + message.username + '</p><p id="message">' + message.text + '</p><p class="room">' + message.roomname + '</p></div>');
+    if (app.users.followers.includes(message.username)) {
+      // $message.toggleClass('friend');
+      $message.toggleClass('friend');
+      // $( "div.tumble" ).toggleClass( "bounce" )
+    }
     $('#chats').append($message);
-    $('.username').on('click', app.handleUsernameClick);
+    
+    
   },
   renderRoom: function(room) {
     $room = '<p>' + room + '</p>';
     $('#roomSelect').append($room);
   },
-  handleUsernameClick: () => true,
-  handleSubmit: () => true
+  handleUsernameClick: function() {
+    app.users.followers.push($(this)[0].innerText); // inner Text is probably bad practice. Change later, maybe.
+    // app.clearMessages();
+    // app.fetch();
+    // return true;
+  },
+  handleSubmit: function(text) {
+    // window.location.search // al;skd=username;
+    //indexOf = 
+    // slice username out
+    console.log('The handleSubmit function is running!');
+        
+  },
+  server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+  addNewTweets: function() {},
+  users: {username: 'us', followers: []}
 };
 
-var message = {
-  username: 'shawndrost',
-  text: 'trololo',
-  roomname: '4chan',
-};
- 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: data,
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan',
+// };
+
+app.init();
+$(document).ready(function() {
+  $('form').on('submit', app.handleSubmit); 
 });
-
-
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-  type: 'GET',
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: data', data.results[0]);
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
-
+// });
